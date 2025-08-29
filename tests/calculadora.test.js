1de8fa31965862f3f4d731fa87fafcb891819e60
Adriana <bussoloadriana@gmail.com>
2025-08-29 10:09:04 -0300

    adicionei a soma e seu teste

---
+++ tests/calculadora.test.js
@@ -0,0 +1,5 @@
+const { somarDoisNumeros } = require('../src/calculadora'); // importação da função
+
+const resultadoDaSoma = somarDoisNumeros (5,3);  // pede resultado
+
+console.log(resultadoDaSoma); // mostra o valor
